8d193000faa21ad702bdafb600d7e5b77e7fcb03
forest93 <forest93@163.com>
2020-08-09 17:04:55 +0800

    CommandLineArgs should exclude 1st arg (executable name).

--- CXuesong.Shims.VisualBasic.ApplicationServices/ConsoleApplicationBase.vb
+++ CXuesong.Shims.VisualBasic.ApplicationServices/ConsoleApplicationBase.vb
@@ -4,7 +4,7 @@ Imports System.Reflection
 Public Class ConsoleApplicationBase
     Inherits ApplicationBase
 
-    Private ReadOnly m_CommandLineArgs As New Lazy(Of IReadOnlyList(Of String))(Function() Environment.GetCommandLineArgs())
+    Private ReadOnly m_CommandLineArgs As New Lazy(Of IReadOnlyList(Of String))(Function() Environment.GetCommandLineArgs().Skip(1).ToArray)
 
     Public Sub New(owningAssembly As Assembly)
         MyBase.New(owningAssembly)
@@ -12,12 +12,14 @@ Public Class ConsoleApplicationBase
 
     ' Public ReadOnly Property Deployment As ApplicationDeployment
 
+    ''' <summary>Gets a collection containing the command-line arguments as strings for the current application.</summary>
     Public ReadOnly Property CommandLineArgs As IReadOnlyList(Of String)
         Get
             Return If(InternalCommandLine, m_CommandLineArgs.Value)
         End Get
     End Property
 
+    ''' <summary>Sets the values to use as the current application's command-line arguments.</summary>
     Public Property InternalCommandLine As ReadOnlyCollection(Of String)
 
     ' Public ReadOnly Property IsNetworkDeployed As Boolean
